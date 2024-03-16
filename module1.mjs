8989b1d79cb9cb862f3e3b6d7d6aab5ed6df5d8d
Ashfaque Mustaque <106698498+ashfaque-mustaque@users.noreply.github.com>
2024-03-16 13:03:44 +0530

    Add files via upload

---
+++ module1.mjs
@@ -0,0 +1,8 @@
+const a="Ashfaque";
+const b="Aman";
+const c="Zeeshan";
+const d="Sonu";
+//it is exporting bydefault
+export default d;
+
+export {b};
---
+++ module2.mjs
@@ -0,0 +1,3 @@
+import ui,{b} from './module1.mjs';
+console.log(ui);
+console.log(b);
